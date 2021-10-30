ebdb02b251f22d147fd7bfd70c2e89957cb1d450
Mohanenfield <83965863+Mohanenfield@users.noreply.github.com>
2021-10-31 02:05:59 +0530

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,140 @@
+const products = [
+  {
+    name
+      :
+      "iPhone 13",
+    imageUrl
+      :
+      "https://9to5mac.com/wp-content/uploads/sites/6/2021/03/Screenshot-2021...",
+    description
+      :
+      "Your new superpower. Buy now. Our most advanced dual-camera system eve...",
+    price
+      :
+      "1099",
+    countInStock
+      :
+      "10",
+  },
+  {
+    name
+      :
+      "Cannon EOS-1D",
+    imageUrl
+      :
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiCFogYftoctCd_g...",
+    description
+      :
+      "The EOS-1D X combines speed with image quality, to create the next gen...",
+    price
+      :
+      "1500",
+    countInStock
+      :
+      "6",
+  },
+  {
+    name
+      :
+      "Amazon Alexa",
+    imageUrl
+      :
+      "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBw8PEA8NDQ0...",
+    description
+      :
+      "Alexa is Amazon's cloud-based voice service available on hundreds of m...",
+    price
+      :
+      "100",
+    countInStock
+      :
+      "30",
+  },
+  {
+    name
+      :
+      "Boat Earpods",
+    imageUrl
+      :
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLZACV0QulgGxXEU...",
+    description
+      :
+      "Wireless earbuds to amplify the music you love and forget what’s happe...",
+    price
+      :
+      "120",
+    countInStock
+      :
+      "40",
+  },
+  {
+    name
+      :
+      "Sony Headphones",
+    imageUrl
+      :
+      "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAPDQ8QEBI...",
+    description
+      :
+      "Enjoy your favourite music with Sony noise cancelling headphones and e...",
+    price
+      :
+      "500",
+    countInStock
+      :
+      "25",
+  },
+  {
+    name
+      :
+      "Samsung galaxy fold",
+    imageUrl
+      :
+      "https://www.cnet.com/a/img/resize/5ca1a7dc96aa1bf0dcbbdd847b4298fd167d...",
+    description
+      :
+      "Today, the future unfolds. Galaxy Fold defies category with the foldab...",
+    price
+      :
+      "999",
+    countInStock
+      :
+      "30",
+  },
+  {
+    name
+      :
+      "Apple Macbook",
+    imageUrl
+      :
+      "https://cdn.mos.cms.futurecdn.net/uWjEogFLUTBc8mSvagdiuP.jpg",
+    description
+      :
+      "Our thinnest, lightest notebook, completely transformed by the Apple M...",
+    price
+      :
+      "1199",
+    countInStock
+      :
+      "5",
+  },
+  {
+    name
+      :
+      "Apple Watch",
+    imageUrl
+      :
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ap...",
+    description
+      :
+      "Apple Watch is a line of smartwatches produced by Apple Inc. It incorp...",
+    price
+      :
+      "500",
+    countInStock
+      :
+      "15",
+  }
+];
+
+module.exports = products;
